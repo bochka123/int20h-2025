ef39a34dd8149e2c242554fb3cdca2d2db84675d
kovol <kovol@smart-it.com>
2025-03-01 00:45:20 +0200

    update auth

--- Int20h2025.Backend/Int20h2025.BLL/BllConfiguration.cs
+++ Int20h2025.Backend/Int20h2025.BLL/BllConfiguration.cs
@@ -1,4 +1,6 @@
-﻿using Int20h2025.BLL.Mappers;
+﻿using Int20h2025.BLL.Interfaces;
+using Int20h2025.BLL.Mappers;
+using Int20h2025.BLL.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Int20h2025.BLL
@@ -7,6 +9,7 @@ namespace Int20h2025.BLL
     {
         public static IServiceCollection ConfigureBllServiceCollection(this IServiceCollection services)
         {
+            services.AddScoped<IProfileService, ProfileService>();
             services.AddAutoMapper(typeof(MapperProfile));
             return services;
         }
