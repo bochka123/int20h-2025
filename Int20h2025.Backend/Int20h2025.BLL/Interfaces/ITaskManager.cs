66c9770dee24932c72fb3ac7ba65c7d6f5c5bb18
Oleksandr Tkach <oletk@smart-it.com>
2025-03-01 19:28:19 +0200

    added dynamic system prompt generation

--- Int20h2025.Backend/Int20h2025.BLL/Interfaces/ITaskManager.cs
+++ Int20h2025.Backend/Int20h2025.BLL/Interfaces/ITaskManager.cs
@@ -1,7 +1,11 @@
-﻿namespace Int20h2025.BLL.Interfaces
+﻿using Int20h2025.Common.Models.Ai;
+
+namespace Int20h2025.BLL.Interfaces
 {
     public interface ITaskManager
     {
-
+        public string SystemName { get; init; }
+        Task<OperationResult> ExecuteMethodAsync(string methodName, object[] parameters);
+        SystemMethodInfo GetAvailableMethods();
     }
 }
