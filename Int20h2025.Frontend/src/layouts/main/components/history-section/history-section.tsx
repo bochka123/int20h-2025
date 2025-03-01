6a9cee420f8bbce100b2f8e6ce4fed29574151ac
YaroslavBugayov <bugayov576@gmail.com>
2025-03-01 17:58:10 +0200

    feature: Styling with very bad colors

--- Int20h2025.Frontend/src/layouts/main/components/history-section/history-section.tsx
+++ Int20h2025.Frontend/src/layouts/main/components/history-section/history-section.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 
+import styles from './history-section.module.scss';
+
 type HistorySectionProps = {}
 const HistorySection: FC<HistorySectionProps> = () => {
     return (
-        <div>
+        <div className={styles.historySectionWrapper}>
             History
         </div>
     );
