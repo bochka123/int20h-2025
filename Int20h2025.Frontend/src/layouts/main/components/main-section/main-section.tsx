80c4b28c3cc324a0ac7c25414c7c26c033f69662
YaroslavBugayov <bugayov576@gmail.com>
2025-03-01 22:48:00 +0200

    feature: Add logout button

--- Int20h2025.Frontend/src/layouts/main/components/main-section/main-section.tsx
+++ Int20h2025.Frontend/src/layouts/main/components/main-section/main-section.tsx
@@ -1,16 +1,20 @@
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightFromBracket, faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { FC, useState } from 'react';
 
 import { IconButton, MultilineInput } from '@/components';
-import { useProcessMutation } from '@/services/';
+import { useLogOutMutation, useProcessMutation } from '@/services';
 
 import styles from './main-section.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 type MainSectionProps = {}
 const MainSection: FC<MainSectionProps> = () => {
 
     const [process] = useProcessMutation();
+    const [logOut] = useLogOutMutation();
     const [isButtonDisabled, setButtonDisabled] = useState(false);
+
+    const navigate = useNavigate();
 
     const [message, setMessage] = useState<string>('');
 
@@ -26,9 +30,17 @@ const MainSection: FC<MainSectionProps> = () => {
                 console.error(err);
             });
     };
+
+    const handleLogOut = (): void => {
+        logOut();
+        navigate('/auth');
+    };
     
     return (
         <div className={styles.mainSectionWrapper}>
+            <div className={styles.header}>
+
+            </div>
             <div className={styles.generalWrapper}>
                 <h1>Bobr intelligence</h1>
                 <div className={styles.inputWrapper}>
@@ -49,6 +61,9 @@ const MainSection: FC<MainSectionProps> = () => {
                         />
                     </div>
                 </div>
+            </div>
+            <div className={styles.footer}>
+                <IconButton icon={faArrowRightFromBracket} onClick={handleLogOut} />
             </div>
         </div>
     );
