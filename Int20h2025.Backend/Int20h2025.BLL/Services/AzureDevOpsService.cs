962e0af37669a8f0864ba1053de23081d5329402
kovol <kovol@smart-it.com>
2025-03-02 04:29:09 +0200

    rewrite create method for azure devops

--- Int20h2025.Backend/Int20h2025.BLL/Services/AzureDevOpsService.cs
+++ Int20h2025.Backend/Int20h2025.BLL/Services/AzureDevOpsService.cs
@@ -2,6 +2,7 @@
 using Int20h2025.BLL.Interfaces;
 using Int20h2025.Common.Exceptions;
 using Int20h2025.Common.Models.Ai;
+using Int20h2025.Common.Models.AzureDevops;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
 using Microsoft.VisualStudio.Services.OAuth;
 using Microsoft.VisualStudio.Services.WebApi;
@@ -19,11 +20,8 @@ namespace Int20h2025.BLL.Services
             switch (methodName)
             {
                 case "CreateTask":
-                    var title = parameters["title"].ToString();
-                    var organizationName = parameters["organizationName"].ToString();
-                    var projectName = parameters["projectName"].ToString();
-                    var assignedTo = parameters["assignedTo"].ToString();
-                    return await CreateTaskAsync(title, organizationName, projectName, assignedTo);
+                    var model = parameters.ToObject<CreateTaskModel>()!;
+                    return await CreateTaskAsync(model);
 
                 case "UpdateTask":
                     var parseUpdateTaskIdsuccess = int.TryParse(parameters[0].ToString(), out var updateTaskId);
@@ -52,13 +50,71 @@ namespace Int20h2025.BLL.Services
             }
         }
 
+        private async Task<OperationResult> CreateTaskAsync(CreateTaskModel model)
+        {
+            var accessToken = userContextService.UserData ?? throw new InternalPointerBobrException("User must sign in devops firstly.");
+            var credentials = new VssOAuthAccessTokenCredential(accessToken);
+            using var connection = new VssConnection(new Uri(_devOpsOrgUrl + model.OrganizationName), credentials);
+            var workItemClient = connection.GetClient<WorkItemTrackingHttpClient>();
+
+            var patchDocument = new JsonPatchDocument
+            {
+                new JsonPatchOperation
+                {
+                    Operation = Microsoft.VisualStudio.Services.WebApi.Patch.Operation.Add,
+                    Path = "/fields/System.Title",
+                    Value = model.Title
+                },
+                new JsonPatchOperation
+                {
+                    Operation = Microsoft.VisualStudio.Services.WebApi.Patch.Operation.Add,
+                    Path = "/fields/System.AssignedTo",
+                    Value = model.AssignedTo
+                }
+            };
+
+            if (!string.IsNullOrEmpty(model.Description))
+            {
+                patchDocument.Add(new JsonPatchOperation
+                {
+                    Operation = Microsoft.VisualStudio.Services.WebApi.Patch.Operation.Add,
+                    Path = "/fields/System.Description",
+                    Value = model.Description
+                });
+            }
+
+            if (!string.IsNullOrEmpty(model.Priority))
+            {
+                patchDocument.Add(new JsonPatchOperation
+                {
+                    Operation = Microsoft.VisualStudio.Services.WebApi.Patch.Operation.Add,
+                    Path = "/fields/Microsoft.VSTS.Common.Priority",
+                    Value = model.Priority
+                });
+            }
+
+            var workItem = await workItemClient.CreateWorkItemAsync(patchDocument, model.ProjectName, "Task");
+
+            return new OperationResult { Response = $"WorkItem: {workItem.Url}, {model.Title}, {model.AssignedTo}", Success = true };
+        }
+
+        private async Task<OperationResult> UpdateTaskAsync(int taskId, string status)
+        {
+            return new OperationResult { Response = $"Задачу з номером '{taskId}' оновлено та призначено статус {status}.", Success = true };
+        }
+
+        private async Task<OperationResult> DeleteTaskAsync(int taskId)
+        {
+            return new OperationResult { Response = $"Задачу з номером '{taskId}' видалено.", Success = true };
+        }
+
         public SystemMethodInfo GetAvailableMethods()
         {
             return new SystemMethodInfo
             {
                 SystemName = SystemName,
                 Methods =
-                    [
+                [
                     new ServiceMethodInfo
                     {
                         MethodName = "CreateTask",
@@ -69,27 +125,45 @@ namespace Int20h2025.BLL.Services
                             {
                                 Name = "title",
                                 Type = "string",
-                                Description = "Task name."
+                                Description = "Task name.",
+                                IsRequired = true
                             },
                             new ParameterInfo
                             {
                                 Name = "projectName",
                                 Type = "string",
-                                Description = "Name of project to create task name."
+                                Description = "Name of project to create task name.",
+                                IsRequired = true
                             },
                             new ParameterInfo
                             {
                                 Name = "organizationName",
                                 Type = "string",
-                                Description = "Name of organization to create task name."
+                                Description = "Name of organization to create task name.",
+                                IsRequired = true
                             },
                             new ParameterInfo
                             {
                                 Name = "assignedTo",
                                 Type = "string",
-                                Description = "Email of user to be assigned."
+                                Description = "Email of user to be assigned.",
+                                IsRequired = true
+                            },
+                            new ParameterInfo
+                            {
+                                Name = "description",
+                                Type = "string",
+                                Description = "Description of the task.",
+                                IsRequired = false
+                            },
+                            new ParameterInfo
+                            {
+                                Name = "priority",
+                                Type = "string",
+                                Description = "Priority of the task.",
+                                IsRequired = false
                             }
-                        ]
+                        ]                   
                     },
                     new ServiceMethodInfo
                     {
@@ -101,13 +175,15 @@ namespace Int20h2025.BLL.Services
                             {
                                 Name = "taskId",
                                 Type = "int",
-                                Description = "Id of task to be updated."
+                                Description = "Id of task to be updated.",
+                                IsRequired = true
                             },
                             new ParameterInfo
                             {
                                 Name = "status",
                                 Type = "string",
-                                Description = "New status that needs to be updated."
+                                Description = "New status that needs to be updated.",
+                                IsRequired = true
                             }
                         ]
                     },
@@ -121,58 +197,13 @@ namespace Int20h2025.BLL.Services
                             {
                                 Name = "taskId",
                                 Type = "int",
-                                Description = "Id of task to be deleted."
+                                Description = "Id of task to be deleted.",
+                                IsRequired = true
                             }
                         ]
                     }
                 ]
             };
-        }
-
-        private async Task<OperationResult> CreateTaskAsync(string title,string organizationName, string projectName, string assignedTo)
-        {
-            var accessToken = userContextService.UserData ?? throw new InternalPointerBobrException("User must sign in devops firstly.");
-            var credentials = new VssOAuthAccessTokenCredential(accessToken);
-            using (var connection = new VssConnection(new Uri(_devOpsOrgUrl + organizationName), credentials))
-            {
-                var workItemClient = connection.GetClient<WorkItemTrackingHttpClient>();
-
-                var patchDocument = new JsonPatchDocument
-                {
-                    new JsonPatchOperation
-                    {
-                        Operation = Microsoft.VisualStudio.Services.WebApi.Patch.Operation.Add,
-                        Path = "/fields/System.Title",
-                        Value = title
-                    },
-                    new JsonPatchOperation
-                    {
-                        Operation = Microsoft.VisualStudio.Services.WebApi.Patch.Operation.Add,
-                        Path = "/fields/System.Description",
-                        Value = "Created via Azure DevOps API with OAuth"
-                    },
-                    new JsonPatchOperation
-                    {
-                        Operation = Microsoft.VisualStudio.Services.WebApi.Patch.Operation.Add,
-                        Path = "/fields/System.AssignedTo",
-                        Value = assignedTo
-                    }
-                };
-
-                var workItem = await workItemClient.CreateWorkItemAsync(patchDocument, projectName, "Task");
-            }
-
-            return new OperationResult { Response = $"Задачу '{title}' створено та призначено {assignedTo}.", Success = true };
-        }
-
-        private async Task<OperationResult> UpdateTaskAsync(int taskId, string status)
-        {
-            return new OperationResult { Response = $"Задачу з номером '{taskId}' оновлено та призначено статус {status}.", Success = true };
-        }
-
-        private async Task<OperationResult> DeleteTaskAsync(int taskId)
-        {
-            return new OperationResult { Response = $"Задачу з номером '{taskId}' видалено.", Success = true };
         }
     }
 }
